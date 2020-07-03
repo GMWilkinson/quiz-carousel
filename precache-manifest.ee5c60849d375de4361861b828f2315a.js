b6b98c414c53b6f3c3ce15a73172a653a06c02fa
Grant Wilkinson <grant@aimermedia.com>
2020-07-03 13:33:09 +0100

    Updates

---
+++ precache-manifest.ee5c60849d375de4361861b828f2315a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1475edda7a8bc96844b3836e41579734",
+    "url": "/quiz-carousel/index.html"
+  },
+  {
+    "revision": "73817d68912e26007e01",
+    "url": "/quiz-carousel/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d345e4d95e57e54e3383",
+    "url": "/quiz-carousel/static/css/main.7eb29335.chunk.css"
+  },
+  {
+    "revision": "73817d68912e26007e01",
+    "url": "/quiz-carousel/static/js/2.e745b08c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quiz-carousel/static/js/2.e745b08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d345e4d95e57e54e3383",
+    "url": "/quiz-carousel/static/js/main.eded8f93.chunk.js"
+  },
+  {
+    "revision": "6f5527e3c40371c7a281",
+    "url": "/quiz-carousel/static/js/runtime-main.f652c0e4.js"
+  },
+  {
+    "revision": "9c7ea99f62050d8c3cd46819343a23a0",
+    "url": "/quiz-carousel/static/media/0.9c7ea99f.gif"
+  },
+  {
+    "revision": "b849b8d4d6b0382ecc6b4f731b13a81f",
+    "url": "/quiz-carousel/static/media/1.b849b8d4.jpg"
+  },
+  {
+    "revision": "777b1c55b212f4d23e83577b871a121b",
+    "url": "/quiz-carousel/static/media/12.777b1c55.jpg"
+  },
+  {
+    "revision": "58dc432704fd78124028d7c7dc73b5d2",
+    "url": "/quiz-carousel/static/media/14.58dc4327.jpg"
+  },
+  {
+    "revision": "37e28ccc9f9c9094fc40766d9fe4248a",
+    "url": "/quiz-carousel/static/media/17.37e28ccc.jpg"
+  },
+  {
+    "revision": "3cb556f765b1ccf75e798e9ded17918f",
+    "url": "/quiz-carousel/static/media/18.3cb556f7.jpg"
+  },
+  {
+    "revision": "4a46f04b15c6a1d278ee65af91222b60",
+    "url": "/quiz-carousel/static/media/19.4a46f04b.jpg"
+  },
+  {
+    "revision": "7f074cbc899960f37699f15e2a47e42b",
+    "url": "/quiz-carousel/static/media/3.7f074cbc.jpg"
+  },
+  {
+    "revision": "8b8da7cfbb017be627845cb7595a59ae",
+    "url": "/quiz-carousel/static/media/4.8b8da7cf.jpg"
+  },
+  {
+    "revision": "8fc3199ca8c8f5ac288f6220e5d30005",
+    "url": "/quiz-carousel/static/media/5.8fc3199c.jpg"
+  },
+  {
+    "revision": "d9c67cdd2f0ad4697f98c8176e2ba1ac",
+    "url": "/quiz-carousel/static/media/8.d9c67cdd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz-carousel/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz-carousel/precache-manifest.71afb321aeaa5316b981d77f86e9dc01.js"
+  "/quiz-carousel/precache-manifest.ee5c60849d375de4361861b828f2315a.js"
 );
 
 self.addEventListener('message', (event) => {
